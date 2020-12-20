ecd85551478b46d6761e2825a0d38d32c317657d
Nancy <nanc.ng1@gmail.com>
2020-12-19 21:53:21 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Nancy Ng  on 12/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
